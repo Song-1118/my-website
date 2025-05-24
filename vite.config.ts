4b7838f778411dcf1b58b3beff6f1f8563fbdbc7
Songziming <19924539884@163.com>
2025-05-24 20:50:33 +0800

    V1.6.3

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,27 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-import { createSitemap } from 'vite-plugin-sitemap'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import Pages from 'vite-plugin-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    Pages(), // 自动生成路由
+    createHtmlPlugin({
+      minify: true, // 压缩 HTML
+      inject: {
+        data: {
+          title: '极光栈|AuroraHub', // 自定义 SEO 标题
+          description: '极光栈主站，有下载板块、网站导航和自研的音乐播放器' // 自定义 SEO 描述
+        }
+      }
+    }),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
     Components({
       resolvers: [ElementPlusResolver()],
     }),
-    createSitemap({
-      baseUrl: 'https://songweb.dpdns.org',
-      routes: [...], // 从你的路由中获取所有路径
-    }),
-  ],
+  ]
 })
